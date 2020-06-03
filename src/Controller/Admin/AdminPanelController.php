c04027fb6da759d536aa459459b947ce1e378d1c
francoisvaillant <frvaillant@gmail.com>
2020-06-03 20:40:46 +0200

    add client add form in admin

--- src/Controller/Admin/AdminPanelController.php
+++ src/Controller/Admin/AdminPanelController.php
@@ -2,21 +2,50 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Client;
+use App\Form\ClientType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use \DateTime;
 
 /**
  * @Route("/admin", name="admin")
  */
 class AdminPanelController extends AbstractController
 {
+
     /**
      * @Route("/", name="admin_dashboard")
+     * @param Request $request
+     * @return Response
      */
-    public function index()
+    public function index(Request $request): Response
     {
-        return $this->render('admin/index.html.twig', [
+        $client = new Client();
+        $formClient = $this->createForm(ClientType::class, $client);
+        $formClient->handleRequest($request);
 
+        if ($formClient->isSubmitted() && $formClient->isValid()) {
+            if (!$client->getCreatedAt()) {
+                $client->setCreatedAt(new DateTime());
+            }
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($client);
+            $entityManager->flush();
+            return $this->render('admin/index.html.twig', [
+                'client' => $client,
+                'form'   => $formClient->createView(),
+                'result' => 'Client ajouté à la base de données'
+            ]);
+
+            return $this->redirectToRoute('admin_dashboard');
+        }
+
+        return $this->render('admin/index.html.twig', [
+            'client' => $client,
+            'form' => $formClient->createView(),
         ]);
     }
 }
