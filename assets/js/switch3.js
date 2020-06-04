61b7cb729c4c00fdeb05a6dda6abb7fc5ca129ae
Amelie <a.grn33@gmail.com>
2020-06-04 18:10:15 +0200

    app.js for switch

--- assets/js/switch3.js
+++ assets/js/switch3.js
@@ -87,9 +87,3 @@ class Switch3 {
         return this;
     }
 }
-
-document.addEventListener('DOMContentLoaded', () => {
-    const switcher = new Switch3(['non', '?', 'oui'], [2, 0, 1], 'switcher', 0,
-        '', 'vehicle_hasCome');
-    switcher.init();
-})
--- assets/js/switchHasCome.js
+++ assets/js/switchHasCome.js
@@ -1,5 +1,6 @@
+
 document.addEventListener('DOMContentLoaded', () => {
-    const switcher = new Switch3(['non', '?', 'oui'], [2, 0, 1], 'switcher', 1,
+    const switcher = new Switch3(['non', '?', 'oui'], [2, 0, 1], 'switcher', 0,
         '', 'vehicle_hasCome');
     switcher.init();
 })
