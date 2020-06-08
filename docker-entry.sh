d87fba3882e9db135679ada69f99f7e718e0dbc0
Guillaume HARARI <guillaume.harari@wildcodeschool.fr>
2020-06-08 16:48:40 +0200

    Add deploy files

---
+++ docker-entry.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+## Symfony cconfiguration
+php bin/console doctrine:database:create --if-not-exists --quiet --no-interaction
+php bin/console doctrine:migrations:migrate --verbose --no-interaction --allow-no-migration
+if [ ${APP_ENV} != "prod" ]
+   then
+        php bin/console doctrine:schema:update --force --verbose --no-interaction
+        php bin/console doctrine:fixtures:load --purge-with-truncate --quiet --no-interaction --no-debug
+ fi
+
+php bin/console cache:clear
+php bin/console cache:warmup
+
+chmod -R 777 /var/www/var
+chmod -R 777 /var/www/public
+
+
+## server config
+php-fpm &
+nginx -g "daemon off;"
