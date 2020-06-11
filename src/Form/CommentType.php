3bbe3debdcbc6c4ec79505e14df939f31b4782b1
Carolinefourcade <caroannab33@gmail.com>
2020-06-11 09:51:48 +0200

    delete identifier

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +13,9 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('identifier')
+            ->add('name', TextType::class, [
+                'label' => 'Commentaires'
+            ])
         ;
     }
 
