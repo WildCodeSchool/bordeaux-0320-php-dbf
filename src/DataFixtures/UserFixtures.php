fee41a0daf377401df249cbcb961f1c67ef8ce92
Elisa BOURG <elisabourg@macbook-air-de-elisa-1.home>
2020-06-15 21:23:56 +0200

    add login

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -3,29 +3,53 @@
 
 namespace App\DataFixtures;
 
+use DateTime;
 use Faker;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
 {
-    const NUMBER_OF_USERS = 30;
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
 
     public function load(ObjectManager $manager)
     {
-        $faker = Faker\Factory::create('fr_FR');
-        for ($i = 0; $i < self::NUMBER_OF_USERS; $i++) {
-            $user = new User();
-            $user->setLastname($faker->lastName);
-            $user->setFirstname($faker->firstName);
-            $user->setEmail($faker->email);
-            $user->setPhone($faker->phoneNumber);
-            $user->setPassword('12345');
-            $user->setCreatedAt($faker->dateTime);
-            $manager->persist($user);
-            $this->addReference('user_' . $i, $user);
-        }
+        // Création d’un utilisateur de type “collaborateur”
+        $collaborator = new User();
+        $collaborator->setEmail('collaborateur@dbf.com');
+        $collaborator->setFirstname('John');
+        $collaborator->setLastname('Doe');
+        $collaborator->setCreatedAt(new DateTime());
+        $collaborator->setRoles(['ROLE_COLLABORATOR']);
+        $collaborator->setPassword($this->passwordEncoder->encodePassword(
+            $collaborator,
+            'collabpassword'
+        ));
+
+        $manager->persist($collaborator);
+
+        // Création d’un utilisateur de type “administrateur”
+        $admin = new User();
+        $admin->setEmail('admin@dbf.com');
+        $admin->setFirstname('Admin');
+        $admin->setLastname('dbf');
+        $admin->setCreatedAt(new DateTime());
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword($this->passwordEncoder->encodePassword(
+            $admin,
+            'adminpassword'
+        ));
+
+        $manager->persist($admin);
+
+        // Sauvegarde des 2 nouveaux utilisateurs :
         $manager->flush();
     }
 }
