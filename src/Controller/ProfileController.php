e1a8c641232a9e1d6b4f1b402dd7f12421adaa7a
Elisa BOURG <elisabourg@macbook-air-de-elisa-1.home>
2020-06-23 09:53:33 +0200

    clean stuff

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -8,34 +8,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
 
 class ProfileController extends AbstractController
 {
-    /**
-     * @Route("/profile", name="profile")
-     */
-    public function index()
-    {
-        return $this->render('edit.html.twig', [
-            'controller_name' => 'ProfileController',
-        ]);
-    }
+
     /**
      * @Route("edit/profile/{id}", name="profile_edit", methods={"GET","POST"})
      * @param Request $request
      * @param User $user
+     * @param UserPasswordEncoder $userPasswordEncoder
      * @return Response
      */
-    public function editProfile(Request $request, User $user): Response
+    public function editProfile(Request $request, User $user, UserPasswordEncoder $userPasswordEncoder): Response
     {
         $form = $this->createForm(UserProfileType::class, $user);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $user = $form->getData();
-            $user->setPassword($form->get('password')->getData());
+            $plainPassword = $form->get('password')->getData();
+            $encodedPassword = $userPasswordEncoder->encodePassword($user, $plainPassword);
+            $user->setPassword($encodedPassword);
             $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('profile_edit');
+            return $this->redirectToRoute('user_index'); //changer plus tard la redirection
         }
 
         return $this->render('profile/edit.html.twig', [
