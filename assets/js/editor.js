d4dd5668a8f7dfeef0dbe5f20b27ca577b96b33c
Carolinefourcade <caroannab33@gmail.com>
2020-07-06 11:52:44 +0200

    Admin Edit subject ok

---
+++ assets/js/editor.js
@@ -0,0 +1,49 @@
+const initializeSelects = () => {
+    const selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+}
+
+const lineHider = (className) => {
+    const lines = document.getElementsByClassName(className);
+    for (let i = 0; i < lines.length; i++) {
+        lines[i].classList.add('hide');
+    }
+};
+
+const btnDesactivator = (className) => {
+    const btns = document.getElementsByClassName(className);
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].classList.remove('btn-floating', 'light-blue');
+    }
+};
+
+const editor = (button, url, classLine, classBtn) => {
+    lineHider(classLine);
+    btnDesactivator(classBtn)
+    button.classList.add('btn-floating', 'light-blue')
+    fetch(url, {
+        method:'GET',
+    }).then((response) => {
+        return response.text()
+    }).then((html) => {
+        const cell = document.getElementById(`subject-td-edit-${button.dataset.target}`);
+        cell.innerHTML = html;
+        initializeSelects();
+    })
+    const editLine = document.getElementById(`subject-line-edit-${button.dataset.target}`);
+    editLine.classList.remove('hide');
+}
+
+
+
+document.addEventListener('DOMContentLoaded',() => {
+    //edition des subjects
+    const editorbtns= document.getElementsByClassName('edit-subject-button');
+    for (let i = 0; i < editorbtns.length; i++) {
+        editorbtns[i].addEventListener('click', (event) => {
+            editor(editorbtns[i], `/subject/${editorbtns[i].dataset.target}/edit`, 'editor-line', 'edit-subject-button')
+        });
+    }
+    // fin edition subjects
+
+}) //end of document
--- src/Controller/CivilityController.php
+++ src/Controller/CivilityController.php
@@ -100,7 +100,7 @@ class CivilityController extends AbstractController
         $entityManager->flush();
 
         $response = new JsonResponse();
-        $status = JsonResponse::HTTP_OK;
+        $status = JsonResponse::HTTP_NO_CONTENT;
         $response->setStatusCode($status);
 
         return $response;
