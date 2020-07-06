2bebbf8e88a68cfe6726a525dd1b8f43d55a4680
Carolinefourcade <caroannab33@gmail.com>
2020-07-06 14:20:08 +0200

    fix subject editing

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -20,28 +20,26 @@ const btnDesactivator = (className) => {
 const editor = (button, url, classLine, classBtn) => {
     lineHider(classLine);
     btnDesactivator(classBtn)
-    button.classList.add('btn-floating', 'light-blue')
+    button.classList.add('btn-floating', 'light-blue');
     fetch(url, {
         method:'GET',
     }).then((response) => {
-        return response.text()
+        return response.text();
     }).then((html) => {
         const cell = document.getElementById(`subject-td-edit-${button.dataset.target}`);
         cell.innerHTML = html;
         initializeSelects();
-    })
+    });
     const editLine = document.getElementById(`subject-line-edit-${button.dataset.target}`);
     editLine.classList.remove('hide');
-}
-
-
+};
 
 document.addEventListener('DOMContentLoaded',() => {
     //edition des subjects
-    const editorbtns= document.getElementsByClassName('edit-subject-button');
-    for (let i = 0; i < editorbtns.length; i++) {
-        editorbtns[i].addEventListener('click', (event) => {
-            editor(editorbtns[i], `/subject/${editorbtns[i].dataset.target}/edit`, 'editor-line', 'edit-subject-button')
+    const editorSubjectButtons = document.getElementsByClassName('edit-subject-button');
+    for (let i = 0; i < editorSubjectButtons.length; i++) {
+        editorSubjectButtons[i].addEventListener('click', (event) => {
+            editor(editorSubjectButtons[i], `/subject/${editorSubjectButtons[i].dataset.target}/edit`, 'editor-line', 'edit-subject-button')
         });
     }
     // fin edition subjects
