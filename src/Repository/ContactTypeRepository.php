a3979228a69b20467092e2b7b4f51e1d9df7d926
francoisvaillant <frvaillant@gmail.com>
2020-07-21 23:25:30 +0200

    removed commented code

--- src/Repository/ContactTypeRepository.php
+++ src/Repository/ContactTypeRepository.php
@@ -18,33 +18,4 @@ class ContactTypeRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, ContactType::class);
     }
-
-    // /**
-    //  * @return ContactType[] Returns an array of ContactType objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('c.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?ContactType
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
