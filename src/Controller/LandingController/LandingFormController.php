2dd26619d3428ff5ef83ef498afc5e5afa21648b
francoisvaillant <frvaillant@gmail.com>
2021-01-29 16:45:53 +0100

    try ip

--- src/Controller/LandingController/LandingFormController.php
+++ src/Controller/LandingController/LandingFormController.php
@@ -61,7 +61,7 @@ class LandingFormController extends AbstractController
             'form' => $landingForm->createView(),
             'errors' => $errors,
             'brand' => $brand,
-            'ipRemote'    => $this->getIp()
+            'ipRemote'    => $_SERVER
         ]);
     }
 
