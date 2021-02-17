2af64cbff0b239512020d29d0971bf6c18473176
francoisvaillant <frvaillant@gmail.com>
2021-02-17 09:57:53 +0100

    fix rgpd command

--- src/Command/RgpdCommand.php
+++ src/Command/RgpdCommand.php
@@ -4,16 +4,23 @@
 namespace App\Command;
 
 
+use App\Events;
 use App\Repository\CallRepository;
 use App\Repository\CallTransferRepository;
 use App\Repository\ClientRepository;
 use App\Repository\ServiceRepository;
 use App\Repository\VehicleRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
+use Symfony\Bundle\SecurityBundle\Tests\Functional\app\AppKernel;
+use Symfony\Component\Mailer\MailerInterface;
 
 class RgpdCommand extends Command
 {
@@ -39,13 +46,19 @@ class RgpdCommand extends Command
      * @var ClientRepository
      */
     private ClientRepository $clientRepository;
+    /**
+     * @var EventDispatcherInterface
+     */
+    private EventDispatcherInterface $eventDispatcher;
 
     public function __construct(
         EntityManagerInterface $entityManager,
         CallRepository $callRepository,
         CallTransferRepository $callTransferRepository,
         VehicleRepository $vehicleRepository,
-        ClientRepository $clientRepository
+        ClientRepository $clientRepository,
+        EventDispatcherInterface $eventDispatcher,
+        MailerInterface $mailer
     ) {
         parent::__construct();
         $this->manager = $entityManager;
@@ -53,6 +66,8 @@ class RgpdCommand extends Command
         $this->callTransferRepository = $callTransferRepository;
         $this->vehicleRepository = $vehicleRepository;
         $this->clientRepository = $clientRepository;
+        $this->eventDispatcher = $eventDispatcher;
+        $this->mailer = $mailer;
     }
 
     protected function configure()
@@ -63,16 +78,20 @@ class RgpdCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
+        $results = [];
 
         $date = new \DateTime('now');
         $date->sub(new \DateInterval('P3Y'))->sub(new \DateInterval('P7D'));
 
-
         $io->title('Suppression des appels de plus de 36 mois');
-            $io->writeln($this->callRepository->removeOldCalls($date) . ' appels supprimés');
+            $calls = $this->callRepository->removeOldCalls($date);
+            $results['calls'] = $calls;
+            $io->writeln($calls . ' appels supprimés');
 
         $io->title('suppression des historiques de transfert de plus plus de 36 mois');
-            $io->writeln($this->callTransferRepository->removeOldCalls($date) . ' transferts supprimés');
+            $transfers = $this->callTransferRepository->removeOldCalls($date);
+            $results['transfers'] = $transfers;
+            $io->writeln($transfers . ' transferts supprimés');
 
         $io->title('Récupération des véhicules qui ne sont plus dans la table des appels');
             $vehicles = $this->vehicleRepository->getOldVehicles();
@@ -80,7 +99,9 @@ class RgpdCommand extends Command
                 $vehicle = $this->vehicleRepository->findOneById($vehicle['id']);
                 $this->manager->remove($vehicle);
             }
+
             $this->manager->flush();
+            $results['vehicles'] = count($vehicles);
             $io->writeln(count($vehicles) . ' véhicules supprimés');
 
         $io->title('Suppression des clients qui n\'ont plus aucun appel les concernant');
@@ -90,7 +111,23 @@ class RgpdCommand extends Command
                 $this->manager->remove($client);
             }
             $this->manager->flush();
+            $results['clients'] = count($clients);
             $io->writeln(count($clients) . ' clients supprimés');
+
+            $dateExec = new \DateTime('now');
+
+        $email = (new TemplatedEmail())
+            ->from($_SERVER['MAILER_FROM_ADDRESS'])
+            ->to($_SERVER['REPORT_DESTINATARY'])
+            ->subject('Compte rendu RGPD BackUp')
+            ->htmlTemplate('emails/template_rgpd_mail.html.twig')
+            ->context([
+                'data'     => $results,
+                'dateexec' => $dateExec,
+                'refdate'  => $date
+            ]);
+
+        $this->mailer->send($email);
     }
 
 }
