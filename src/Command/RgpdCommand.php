7cf14daa1f8346f2a1c14594db8d42ab3c18a0d4
francoisvaillant <frvaillant@gmail.com>
2021-02-18 21:16:24 +0100

    update rgpd command

--- src/Command/RgpdCommand.php
+++ src/Command/RgpdCommand.php
@@ -10,10 +10,12 @@ use App\Repository\ClientRepository;
 use App\Repository\ServiceRepository;
 use App\Repository\VehicleRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Mailer\MailerInterface;
 
 class RgpdCommand extends Command
 {
@@ -39,13 +41,18 @@ class RgpdCommand extends Command
      * @var ClientRepository
      */
     private ClientRepository $clientRepository;
+    /**
+     * @var MailerInterface
+     */
+    private MailerInterface $mailer;
 
     public function __construct(
         EntityManagerInterface $entityManager,
         CallRepository $callRepository,
         CallTransferRepository $callTransferRepository,
         VehicleRepository $vehicleRepository,
-        ClientRepository $clientRepository
+        ClientRepository $clientRepository,
+        MailerInterface $mailer
     ) {
         parent::__construct();
         $this->manager = $entityManager;
@@ -53,6 +60,7 @@ class RgpdCommand extends Command
         $this->callTransferRepository = $callTransferRepository;
         $this->vehicleRepository = $vehicleRepository;
         $this->clientRepository = $clientRepository;
+        $this->mailer = $mailer;
     }
 
     protected function configure()
@@ -64,15 +72,23 @@ class RgpdCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
 
-        $date = new \DateTime('now');
+        $results = [];
+
+        $date  = new \DateTime('now');
+        $today = clone $date;
+
         $date->sub(new \DateInterval('P3Y'))->sub(new \DateInterval('P7D'));
 
 
         $io->title('Suppression des appels de plus de 36 mois');
-            $io->writeln($this->callRepository->removeOldCalls($date) . ' appels supprimés');
+        $nbCalls = $this->callRepository->removeOldCalls($date);
+            $io->writeln($nbCalls . ' appels supprimés');
+            $results['calls'] = $nbCalls;
 
-        $io->title('suppression des historiques de transfert de plus plus de 36 mois');
-            $io->writeln($this->callTransferRepository->removeOldCalls($date) . ' transferts supprimés');
+            $io->title('suppression des historiques de transfert de plus plus de 36 mois');
+            $nbTransfers = $this->callTransferRepository->removeOldCalls($date);
+            $io->writeln($nbTransfers . ' transferts supprimés');
+            $results['transfers'] = $nbTransfers;
 
         $io->title('Récupération des véhicules qui ne sont plus dans la table des appels');
             $vehicles = $this->vehicleRepository->getOldVehicles();
@@ -81,7 +97,9 @@ class RgpdCommand extends Command
                 $this->manager->remove($vehicle);
             }
             $this->manager->flush();
-            $io->writeln(count($vehicles) . ' véhicules supprimés');
+            $nbVehicles = count($vehicles);
+            $io->writeln($nbVehicles . ' véhicules supprimés');
+            $results['vehicles'] = $nbVehicles;
 
         $io->title('Suppression des clients qui n\'ont plus aucun appel les concernant');
             $clients = $this->clientRepository->getOldClients();
@@ -90,7 +108,21 @@ class RgpdCommand extends Command
                 $this->manager->remove($client);
             }
             $this->manager->flush();
-            $io->writeln(count($clients) . ' clients supprimés');
+            $nbClients = count($clients);
+            $io->writeln($nbClients . ' clients supprimés');
+            $results['clients'] = $nbClients;
+
+        $email = (new TemplatedEmail())
+            ->from($_SERVER['MAILER_FROM_ADDRESS'])
+            ->to($_SERVER['REPORT_DESTINATARY'])
+            ->subject('Rapport de maintenance RGPD de la base Easy Auto')
+            ->htmlTemplate('emails/template_rgpd.html.twig')
+            ->context([
+                'date' => $today,
+                'data' => $results,
+                'ref'  => $date
+            ]);
+        $this->mailer->send($email);
 
         return self::SUCCESS;
     }
