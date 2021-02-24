322e3e903cc3f3c3a40b7c303fc14ca5cf776178
francoisvaillant <frvaillant@gmail.com>
2021-02-24 02:11:54 +0100

    start organization

---
+++ src/Controller/Organization/OrganizationController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\Organization;
+
+use App\Repository\CityRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OrganizationController extends AbstractController
+{
+    /**
+     * @Route("/organization", name="organization")
+     */
+    public function index(CityRepository $cityRepository): Response
+    {
+        return $this->render('organization/index.html.twig', [
+           'cities' => $cityRepository->findBy([], ['name' => 'ASC'])
+        ]);
+    }
+}
