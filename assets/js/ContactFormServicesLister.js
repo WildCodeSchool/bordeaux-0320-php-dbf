07a93d1010c2d0663a6e5833ffa082d94d794bd5
francoisvaillant <frvaillant@gmail.com>
2021-02-24 11:48:53 +0100

    fix alert JS

--- assets/js/ContactFormServicesLister.js
+++ assets/js/ContactFormServicesLister.js
@@ -8,12 +8,24 @@ if (document.getElementById('contact_form_id')) {
     concessionSelector.addEventListener('change', () => {
         let errors = 0;
         if (document.getElementById('dbf_contact_name').value === '') {
+            document.getElementById('dbf_contact_name').classList.add('red')
+            document.getElementById('dbf_contact_name').addEventListener('click', () => {
+                document.getElementById('dbf_contact_name').classList.remove('red')
+            })
             errors++;
         }
         if (document.getElementById('dbf_contact_phone').value === '') {
+            document.getElementById('dbf_contact_phone').classList.add('red')
+            document.getElementById('dbf_contact_phone').addEventListener('click', () => {
+                document.getElementById('dbf_contact_phone').classList.remove('red')
+            })
             errors++
         }
         if (document.getElementById('dbf_contact_immat').value === '') {
+            document.getElementById('dbf_contact_immat').classList.add('red')
+            document.getElementById('dbf_contact_immat').addEventListener('click', () => {
+                document.getElementById('dbf_contact_immat').classList.remove('red')
+            })
             errors++
         }
         if(concessionSelector.value === null) {
