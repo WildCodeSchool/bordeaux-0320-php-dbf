f9c6fe2fcdc6d9ed4c56b2ae5953f52f110e30d6
francoisvaillant <frvaillant@gmail.com>
2021-05-01 16:44:48 +0200

    update backup command

--- src/BackUpService/BackupCommand.php
+++ src/BackUpService/BackupCommand.php
@@ -11,17 +11,23 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\HttpKernel\KernelInterface;
 use \DateTime;
+use BackupManager\Manager;
+use BackupManager\Filesystems\Destination;
 
 class BackupCommand extends Command
 {
     private string $folder;
     private $dropboxService;
+    /**
+     * @var Manager
+     */
+    private Manager $backupManager;
 
-    public function __construct(string $name = null, KernelInterface $kernel, DropBoxService $dropboxService)
+    public function __construct(string $name = null, KernelInterface $kernel, DropBoxService $dropboxService, Manager $backupManager)
     {
         $this->folder = $kernel->getProjectDir() . '/backup';
         $this->dropboxService = $dropboxService;
-
+        $this->backupManager = $backupManager;
         parent::__construct($name);
     }
 
@@ -32,26 +38,27 @@ class BackupCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $command = $this->getApplication()->find('backup-manager:backup');
 
-        $date = new DateTime('now');
-        $h = $date->format('H');
-
-        if (is_file($this->folder . '/backup-' . $h . '.sql.gz')) {
-            unlink($this->folder . '/backup-' . $h . '.sql.gz');
-        }
-
-        $arguments = [
-            'database' => 'development',
-            'destinations' => ['local', 'ftp'],
-            '-c'     => 'gzip',
-            '--filename'  => 'backup-' . $h . '.sql',
-        ];
-
-        $in = new ArrayInput($arguments);
-        $returnCode = $command->run($in, $output);
+        $this->deleteFtpBackup();
+        $this->backupManager->makeBackup()->run('development', [new Destination('ftp', 'backup.sql')], 'gzip');
 
         return Command::SUCCESS;
+    }
+
+    private function deleteFtpBackup()
+    {
+        $file = $_SERVER['FTP_FOLDER'] . '/backup.sql.gz';
+        $conn_id = ftp_connect($_SERVER['FTP_HOST']);
+        $login_result = ftp_login($conn_id, $_SERVER['FTP_USER'], $_SERVER['FTP_PASSWORD']);
+
+        if (ftp_delete($conn_id, $file)) {
+            ftp_close($conn_id);
+            return "sauvegarde effacée avec succès";
+        } else {
+            ftp_close($conn_id);
+            return "Impossible d'effacer le fichier";
+        }
+
     }
 
 }
