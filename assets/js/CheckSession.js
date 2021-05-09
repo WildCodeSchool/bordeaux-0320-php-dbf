d3ffc04d45edcc836a93b4fd83c9902945f61ea8
francoisvaillant <frvaillant@gmail.com>
2021-05-09 22:46:09 +0200

    fix js

--- assets/js/CheckSession.js
+++ assets/js/CheckSession.js
@@ -1,17 +1,10 @@
-const timeout = () => {
-    setTimeout(() => {
-        window.location.href = '/logout'
-    }, 60000)
-}
-
 document.addEventListener('click', () => {
     console.log(window.location.href)
-    clearTimeout(timeout)
-    timeout()
 
     fetch('/checksession', {
         method: 'GET'
     }).then(response => {
+
         if(response.status === 403 && window.location.href !== 'https://easy-autos.fr/') {
                 window.location.href = '/'
         }
