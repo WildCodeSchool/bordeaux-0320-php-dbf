6504a46a94dfc2c9ca6aa0ac30e83bd82c4fe8d8
francoisvaillant <frvaillant@gmail.com>
2021-05-09 19:02:28 +0200

    redirect if session expired

--- assets/js/CheckSession.js
+++ assets/js/CheckSession.js
@@ -3,10 +3,8 @@ document.addEventListener('click', () => {
     fetch('/checksession', {
         method: 'GET'
     }).then(response => {
-        console.log(window.location.href)
         if(response.status === 403) {
             if (window.location.href !== 'https://easy-autos.fr/') {
-                console.log('home raté')
                 window.location.href = '/'
             }
         }
