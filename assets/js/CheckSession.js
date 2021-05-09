84ec1c5256f6e59ebc2eb6fb00cc6c52fc2e05ef
francoisvaillant <frvaillant@gmail.com>
2021-05-09 22:39:31 +0200

    fix js

--- assets/js/CheckSession.js
+++ assets/js/CheckSession.js
@@ -1,12 +1,19 @@
+const timeout = () => {
+    setTimeout(() => {
+        window.location.href = '/logout'
+    }, 60000)
+}
+
 document.addEventListener('click', () => {
+
+    clearTimeout(timeout)
+    timeout()
 
     fetch('/checksession', {
         method: 'GET'
     }).then(response => {
-        if(response.status === 403) {
-            if (window.location.href !== 'https://easy-autos.fr/') {
+        if(response.status === 403 && window.location.href !== 'https://easy-autos.fr/') {
                 window.location.href = '/'
-            }
         }
     })
 
