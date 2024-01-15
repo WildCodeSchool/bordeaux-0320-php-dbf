2e6041678096f5d1c089edb14e86e993ab24978c
François VAILLANT <frvaillant@gmail.com>
2024-01-15 18:39:20 +0100

    Add autofocus on search fields

--- assets/js/userAdmin.js
+++ assets/js/userAdmin.js
@@ -1,11 +1,22 @@
 import { AjaxTools } from './clientFormAdmin';
 
 document.addEventListener('DOMContentLoaded', () => {
+
+    const searchModalElement = document.getElementById('modal-search-user')
+    const modal = M.Modal.getInstance(searchModalElement)
+
+    if(modal) {
+        modal.options.onOpenEnd = () => {
+            console.warn('open')
+            document.getElementById('autocomplete-user-name').focus()
+        }
+    }
     const autocompleteUsers = document.getElementById('autocomplete-user-name');
     const userAjaxer = new AjaxTools('/user/new', '/user/liste');
 
     // Recherche user ///////////////////////////////////////////////////
     if (autocompleteUsers) {
+
         userAjaxer.getData((data) => {
                 M.Autocomplete.init(
                     autocompleteUsers,
