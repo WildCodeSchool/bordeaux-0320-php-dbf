ca930c5863769f3152572e37496346867520a3ca
François VAILLANT <frvaillant@gmail.com>
2024-01-15 20:47:58 +0100

    Fix call transfers

--- assets/js/transferCall.js
+++ assets/js/transferCall.js
@@ -11,27 +11,51 @@ const closeList = (target) => {
 
 const updateTotalCallToProcess = (method = 'add') => {
     const counter = document.getElementById('nb-calls-to-process');
-    let total = parseInt(counter.innerHTML);
-    if (method === 'dec') {
-        total--
-    } else {
-        total++
+    if(counter) {
+        let total = parseInt(counter.innerHTML);
+        if (method === 'dec') {
+            total--
+        } else {
+            total++
+        }
+        counter.innerHTML = total
     }
-    counter.innerHTML = total
 }
 
 const updateTotalCallInProcess = (method = 'add') => {
     const counter = document.getElementById('nb-calls-in-process');
-    let total = parseInt(counter.innerHTML);
-    if (method === 'dec') {
-        total--
-    } else {
-        total++
+    if(counter) {
+        let total = parseInt(counter.innerHTML);
+        if (method === 'dec') {
+            total--
+        } else {
+            total++
+        }
+        counter.innerHTML = total;
     }
-    counter.innerHTML = total;
 }
 
-const transferCall = (callId, userId) => {
+const refreshSlider = (service) => {
+    const target = document.getElementById('slide-out-' + service)
+    if(target) {
+        M.toast({html: 'Rechargement du volet en cours', classes:'orange'})
+
+        fetch('/head/supervision/' + service, {
+
+        }).then(response => {
+            return response.text()
+        }).then(html => {
+            target.innerHTML = html
+            const ev = new CustomEvent('sliderIsOpen')
+            ev.slider = target
+            document.dispatchEvent(ev)
+            M.toast({html: 'Rechargement du volet terminé', classes:'light-green'})
+        })
+    }
+}
+
+const  transferCall =  (callId, userId, callback) => {
+                    M.toast({html: 'Transfert en cours', classes:'orange'})
                     const url    = `/call/process/${callId}/transferto/${userId}`;
                     const params = {
                         method: "get"
@@ -46,40 +70,61 @@ const transferCall = (callId, userId) => {
                                 } else {
                                     updateTotalCallInProcess('dec')
                                 }
-                                M.toast({html: 'Appel transféré', classes:'red'})
+                                M.toast({html: 'Appel transféré', classes:'green'})
                                 document.getElementById(`users-in-service-${callId}`).classList.add('hide');
                                 callLine.classList.add('hide');
+                                callback()
                             }
+                            return false
                         })
                 }
 
+                const init = (sourceElement = null) => {
 
+                    const source = sourceElement ?? document
+
+                    const closers= source.getElementsByClassName('close-list');
+                    for (let i = 0; i<closers.length; i++) {
+                            closers[i].addEventListener('click', (e) => {
+                                e.preventDefault()
+                                const target = closers[i].getAttribute('data-target');
+                                closeList(target)
+                            })
+
+                    }
+
+                    const recipientsList = source.getElementsByClassName('recipient-changer');
+                    for (let i = 0; i<recipientsList.length; i++) {
+
+                            recipientsList[i].addEventListener('click', (e) => {
+                                e.preventDefault()
+                                hideUsersList()
+                                const callId = e.target.dataset.call
+                                document.getElementById(`users-in-service-${callId}`).classList.remove('hide')
+                            })
+                    }
+
+                    const recipients = source.getElementsByClassName('recipients-to-transfer');
+                    for (let i = 0; i<recipients.length; i++) {
+                            recipients[i].addEventListener('click', (e) => {
+                                e.preventDefault();
+
+                                transferCall(e.target.dataset.call, e.target.dataset.for,  () => {
+                                    const isInSlider = recipients[i].dataset.inSlider && recipients[i].dataset.inSlider === '1'
+                                    const service = recipients[i].dataset.service
+                                    if (isInSlider) {
+                                        refreshSlider(service)
+                                    }
+                                })
+
+
+                            })
+                    }
+                }
 document.addEventListener('DOMContentLoaded', () => {
-
-    const closers= document.getElementsByClassName('close-list');
-    for (let i = 0; i<closers.length; i++) {
-        closers[i].addEventListener('click', (e) => {
-            e.preventDefault()
-            const target = closers[i].getAttribute('data-target');
-            closeList(target)
-        })
-    }
-
-    const recipientsList = document.getElementsByClassName('recipient-changer');
-    for (let i = 0; i<recipientsList.length; i++) {
-        recipientsList[i].addEventListener('click', (e) => {
-            e.preventDefault()
-            hideUsersList()
-            const callId = e.target.dataset.call
-            document.getElementById(`users-in-service-${callId}`).classList.remove('hide')
-        })
-    }
-
-    const recipients = document.getElementsByClassName('recipients-to-transfer');
-    for (let i = 0; i<recipients.length; i++) {
-        recipients[i].addEventListener('click', (e) => {
-            e.preventDefault();
-            transferCall(e.target.dataset.call, e.target.dataset.for)
-        })
-    }
+    init()
 })
+document.addEventListener('sliderIsOpen', (ev) => {
+    init(ev.slider)
+})
+
